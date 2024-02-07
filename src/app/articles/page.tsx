5e8b99b1281ebee01c81ef7a74f814bfda82659b
b7pro <biennetk@gmail.com>
2024-02-07 10:35:06 +0100

    new commit

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -3,6 +3,9 @@ import {Article} from "@/utils/type"
 const ArticlesPage = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const result: Article[] = await response.json();
+  if(!response.ok){
+    throw new Error("Fialed to fetch articales")
+  }
 
   return (
     <>
---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+const ErrorPage = () => {
+    return ( 
+        <>
+            <h1>Error page</h1>
+        </>
+     );
+}
+ 
+export default ErrorPage;
