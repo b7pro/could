2f42d3f78c58e8845d2fa582467d0d19c05cb021
b7pro <biennetk@gmail.com>
2024-02-09 08:19:08 +0100

    new commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+"use client"
+const NotFoundPage = () => {
+    return ( 
+        <>
+            <h1> page Not found</h1>
+            <h1>404</h1>
+        </>
+     );
+}
+ 
+export default NotFoundPage;
