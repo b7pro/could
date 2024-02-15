2915390a57fc975510fa76af6b3c577c0052b3ef
b7pro <biennetk@gmail.com>
2024-02-15 09:55:32 +0100

    new commit

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,11 @@
 "use client";
-
-
+import Link from "next/link";
 
 const ErrorPage = () => {
     return ( 
         <>
             <h1>Error page</h1>
-            
+            <Link href={"/"}>go to home</Link>
         </>
      );
 }
